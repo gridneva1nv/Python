4c91d4c3c8ffe12fee12d44ef21f11cbc6f077c8
Hope <gridneva1@gmail.com>
2026-05-22 19:07:08 +0300

    Переменные, списки и функции в Python - ДЗ

--- Lesson_01/lesson_1_task_3.py
+++ Lesson_01/lesson_1_task_3.py
@@ -1,3 +1,3 @@
-fist_name = input('Ваше имя:')
-last_name = input('Ваша фамилия')
-print("Вас зовут: "+last_name+" "+fist_name)
+first_name = input('Ваше имя:')
+last_name = input('Ваша фамилия:')
+print("Вас зовут: "+last_name+" "+first_name)
