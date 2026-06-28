b334726a35c144140b723970d0820cd500ef9301
Hope <gridneva1@gmail.com>
2026-06-28 22:42:40 +0300

    Задача по автоматизации UI-сценария - Контрольная

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -4,10 +4,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
+
 def test_02_calc():
     # 1 Откройте страницу: https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html в Google Chrome.
     driver = webdriver.Chrome()
-    wait = WebDriverWait(driver, 45)
+    wait = WebDriverWait(driver, 50)
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
     # 2 В поле ввода по локатору #delay введите значение 45.
@@ -20,7 +21,7 @@ def test_02_calc():
     # 3 Нажмите на кнопки: 7 + 8 =
     keys = ["7", "+", "8", "="]
     for key in keys:
-        print(f"//span[text() = '{key}']")
+        # print(f"//span[text() = '{key}']")
         button = wait.until(EC.element_to_be_clickable(
             (By.XPATH, f"//span[text() = '{key}']")
             ))
@@ -28,10 +29,10 @@ def test_02_calc():
 
     # 4 Проверьте (assert), что в окне отобразится результат 15 через 45 секунд.
     start = time.perf_counter()
-    wait.until(lambda d: d.find_element(By.CSS_SELECTOR, ".screen").text.strip() == "15")
+    wait.until(lambda d: d.find_element(By.CLASS_NAME, "screen").text.strip() == "15")
     timer = time.perf_counter() - start
-    result = driver.find_element(By.CLASS_NAME, "screen").text.strip()
+    # result = driver.find_element(By.CLASS_NAME, "screen").text.strip()
+    # print(result, timer)
 
-    print(result, timer)
     assert int(timer) == 45
     driver.quit()
